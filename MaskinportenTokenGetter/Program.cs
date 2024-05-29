12403315f95682a903718bfcb8ebadeca693f5f7
Christer Rustand <cbrustand@hotmail.com>
2024-05-29 22:21:33 +0200

    implement temporary insecure handling of credentials

--- MaskinportenTokenGetter/Program.cs
+++ MaskinportenTokenGetter/Program.cs
@@ -4,14 +4,13 @@ using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
-using Microsoft.Extensions.Configuration;
+using System.Text.Json;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json.Linq;
 
-var builder = new ConfigurationBuilder()
-    .AddUserSecrets<Program>();
+var credentialsStore = new CredentialsStore();
 
-var config = builder.Build();
+credentialsStore.Load();
 
 var rootCommand = new RootCommand();
 
@@ -23,6 +22,11 @@ var scopesOption = new Option<string[]>("--scopes")
 };
 
 var tokenTypeOption = new Option<string>("--type")
+{
+    IsRequired = true
+};
+
+var credentialsOption = new Option<string>("--credentials")
 {
     IsRequired = true
 };
@@ -39,44 +43,71 @@ environmentOption.FromAmong("test", "prod");
 generateCommand.AddOption(scopesOption);
 generateCommand.AddOption(tokenTypeOption);
 generateCommand.AddOption(environmentOption);
+generateCommand.AddOption(credentialsOption);
 
-generateCommand.SetHandler(HandleTokenCommand, tokenTypeOption, scopesOption, environmentOption);
+generateCommand.SetHandler(HandleTokenCommand, tokenTypeOption, scopesOption, environmentOption, credentialsOption);
 
 rootCommand.AddCommand(generateCommand);
 
+var credentialsCommand = new Command("credentials");
+
+var addCredentialsCommand = new Command("add");
+
+var addCredentialsNameArgument = new Argument<string>("credential-set name");
+
+var addCredentialsClientIdOption = new Option<Guid>("--client-id")
+{
+    IsRequired = true
+};
+
+var addCredentialsEncodedJwkOption = new Option<string>("--encoded-jwk")
+{
+    IsRequired = true
+};
+
+addCredentialsCommand.AddArgument(addCredentialsNameArgument);
+addCredentialsCommand.AddOption(addCredentialsClientIdOption);
+addCredentialsCommand.AddOption(addCredentialsEncodedJwkOption);
+
+addCredentialsCommand.SetHandler(HandleAddCredentialsCommand, addCredentialsNameArgument, addCredentialsClientIdOption, addCredentialsEncodedJwkOption);
+
+credentialsCommand.AddCommand(addCredentialsCommand);
+
+var removeCredentialsCommand = new Command("remove");
+
+var removeCredentialsNameArgument = new Argument<string>("credential-set name");
+
+removeCredentialsCommand.AddArgument(removeCredentialsNameArgument);
+
+removeCredentialsCommand.SetHandler(HandleRemoveCredentialsCommand, removeCredentialsNameArgument);
+
+credentialsCommand.AddCommand(removeCredentialsCommand);
+
+var listCredentialsCommand = new Command("list");
+
+listCredentialsCommand.SetHandler(HandleListCredentialsCommand);
+
+credentialsCommand.AddCommand(listCredentialsCommand);
+
+rootCommand.AddCommand(credentialsCommand);
+
 rootCommand.Invoke(args);
 
+if (credentialsStore.HasPendingChanges)
+    credentialsStore.Save();
 
-void HandleTokenCommand(string tokenType, string[] scopes, string environment)
+void HandleTokenCommand(string tokenType, string[] scopes, string environment, string credentialsSetName)
 {
-    string? encodedJwk;
-    string? clientId;
-
-    if (environment == "prod")
+    var credentialSet = credentialsStore.Get(credentialsSetName);
+    if (credentialSet is null)
     {
-        encodedJwk = config.GetValue<string>("ProdEncodedJwk");
-        clientId = config.GetValue<string>("ProdClientId");
-    }
-    else
-    {
-        encodedJwk = config.GetValue<string>("EncodedJwk");
-        clientId = config.GetValue<string>("ClientId");
-    }
-
-    if (string.IsNullOrWhiteSpace(clientId))
-    {
-        Console.WriteLine("Client ID was null or whitespace.");
+        Console.WriteLine($"Credentials set with name {credentialsSetName} could not be found");
         return;
     }
     
-    if (string.IsNullOrWhiteSpace(encodedJwk))
-    {
-        Console.WriteLine("Encoded JWK was null or whitespace");
-        return;
-    }
     var jti = Guid.NewGuid().ToString();
 
-    var jwkString = Encoding.UTF8.GetString(Convert.FromBase64String(encodedJwk));
+    var jwkString = Encoding.UTF8.GetString(Convert.FromBase64String(credentialSet.EncodedJwk));
     var jwk = new JsonWebKey(jwkString);
 
     var rsa = RSA.Create();
@@ -101,7 +132,7 @@ void HandleTokenCommand(string tokenType, string[] scopes, string environment)
     var claims = new List<Claim>()
     {
         new("aud", aud),
-        new("iss", clientId!),
+        new("iss", credentialSet.ClientId.ToString()),
         new("exp", DateTimeOffset.UtcNow.AddMinutes(2).ToUnixTimeSeconds().ToString()),
         new("iat", DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString()),
         new("jti", jti)
@@ -165,6 +196,109 @@ void HandleTokenCommand(string tokenType, string[] scopes, string environment)
     var responseContent2 = response2.Content.ReadAsStringAsync().Result; // TODO: Fix
 
     Console.WriteLine(responseContent2);
-    
-    
 }
+
+void HandleAddCredentialsCommand(string name, Guid clientId, string encodedJwk)
+{
+    // Validate
+    ArgumentException.ThrowIfNullOrWhiteSpace(name);
+
+    if (clientId == Guid.Empty)
+        throw new ArgumentException("clientId must be an initialized guid.", nameof(clientId));
+    
+    ArgumentException.ThrowIfNullOrWhiteSpace(encodedJwk);
+    
+    // Do
+    var success = credentialsStore.TryAdd(name, clientId, encodedJwk);
+
+    // Report
+    if (!success)
+        Console.WriteLine("Failed to set credentials");
+}
+
+void HandleRemoveCredentialsCommand(string name)
+{
+    // Validate
+    ArgumentException.ThrowIfNullOrWhiteSpace(name);
+
+    // Do
+    var success = credentialsStore.Remove(name);
+    
+    // Report
+    if (!success)
+        Console.WriteLine("Failed to remove credentials");
+}
+
+void HandleListCredentialsCommand()
+{
+    var credentialSets = credentialsStore.GetAll().ToArray();
+    
+    for (var i = 0; i < credentialSets.Length; i++)
+    {
+        var credentialSet = credentialSets[i];
+        Console.WriteLine(credentialSet.Name);
+        Console.WriteLine(credentialSet.ClientId);
+        Console.WriteLine(credentialSet.EncodedJwk);
+        
+        if (i != credentialSets.Length - 1)
+            Console.WriteLine("-------------------");
+    }
+}
+
+public class CredentialsStore
+{
+    private readonly List<CredentialSet> _store = new();
+    
+    public bool HasPendingChanges { get; private set; }
+    
+    public void Load()
+    {
+        if (File.Exists("credentials.json") == false)
+            File.WriteAllText("credentials.json", "[]");
+        
+        var json = File.ReadAllText("credentials.json");
+        if (string.IsNullOrWhiteSpace(json))
+            throw new Exception("Failed to read credentials.json");
+        
+        var credentialSets = JsonSerializer.Deserialize<List<CredentialSet>>(json);
+        if (credentialSets is null)
+            throw new Exception("Failed to deserialize credentials.json");
+        
+        _store.Clear();
+        
+        _store.AddRange(credentialSets);
+    }
+    
+    public void Save()
+    {
+        var json = JsonSerializer.Serialize(_store);
+        File.WriteAllText("credentials.json", json);
+    }
+    
+    public bool TryAdd(string name, Guid clientId, string encodedJwk)
+    {
+        if (_store.Any(set => set.Name == name))
+            return false;
+        
+        _store.Add(new CredentialSet(name, clientId, encodedJwk));
+        HasPendingChanges = true;
+        return true;
+    }
+
+    public bool Remove(string name)
+    {
+        var success = _store.RemoveAll(set => set.Name == name) > 0;
+        if (success)
+            HasPendingChanges = true;
+        return success;
+    }
+
+    public List<CredentialSet> GetAll() => _store;
+
+    public CredentialSet? Get(string name)
+    {
+        return _store.FirstOrDefault(set => set.Name == name);
+    }
+}
+
+public record CredentialSet(string Name, Guid ClientId, string EncodedJwk);
